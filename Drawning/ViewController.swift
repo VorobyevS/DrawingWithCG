102a4af42fd2fe919c72eaa5e63db29863f89831
Hela <semen1221_123@mail.ru>
2018-10-05 01:57:25 +0300

    First part is done, counter is working

--- Drawning/ViewController.swift
+++ Drawning/ViewController.swift
@@ -10,10 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterView: CounterView!
+    @IBOutlet weak var counterLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        counterLabel.text = "1"
     }
+    
+    @IBAction func buttonsPressed(_ sender: PushButton) {
+        if sender.isAddButton && counterView.counter < 8{
+            counterView.counter += 1
+        }
+        else if !sender.isAddButton && counterView.counter > 0{
+            counterView.counter -= 1
+        }
+        counterLabel.text = String(counterView.counter)
+    }
+    
 
 }
 
